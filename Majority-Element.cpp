4354b2d865f634c7b36451556dccb27fee9cb282
Vikas Singh <vikas.technocrate@gmail.com>
2025-03-16 22:47:33 +0530

    [169] [Time Beats: 34.06%] [Memory Beats: 58.75%] - LeetPush

---
+++ Majority-Element.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+
+        //BRUTE FORCE APPROACH - T.C = O(N^2)
+
+        // int freq  = 0;
+        // int n = nums.size();
+        // for(int val : nums){
+        //     for(int el : nums){
+        //         if(val == el){
+        //             freq++;
+        //         }
+        //     if(freq > n){
+        //         return el;
+        //     }
+        //     }
+        // }
+        // return -1;
+
+        //OPTIMAL FORCE APPROACH - T.C = O(N)
+        // sort(nums.begin(),nums.end());
+        // int freq = 1;
+        // int ans = nums[0];
+        // for(int i  = 1; i  <nums.size();i++){
+            
+        //     if(nums[i] == nums[i-1]){
+        //         freq++;
+        //     }
+        //     else{
+        //         freq = 1;
+        //         ans = nums[i];
+        //     }
+        //     if(freq > nums.size()){
+        //         return ans;;
+        //     }
+        // }
+        // return ans;
+
+        //MOORES'S VOTING ALGORITHM - T.C = O(N)
+        int freq = 0;
+        int ans = 0;
+        for(int i = 0;i < nums.size();i++){
+            if(freq == 0){
+                ans = nums[i];
+            }
+            if(ans == nums[i]){
+                freq++;
+            }
+            else{
+                freq--;
+            }
+        }
+        return ans;
+
+    }
+};
