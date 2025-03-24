d4d13bf3eafa153574138bf318318537104bb225
Vikas Singh <vikas.technocrate@gmail.com>
2025-03-24 11:12:18 +0530

    [3] [Time Beats: 77.34%] [Memory Beats: 58.61%] - LeetPush

---
+++ Longest-Substring-Without-Repeating-Characters.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char, int> charIndex;
+        int left = 0, maxLength = 0;
+        
+        for (int right = 0; right < s.length(); right++) {
+            if (charIndex.find(s[right]) != charIndex.end()) {
+                left = max(left, charIndex[s[right]] + 1);
+            }
+            
+            charIndex[s[right]] = right;
+            maxLength = max(maxLength, right - left + 1);
+        }
+        
+        return maxLength;
+    }
+};
