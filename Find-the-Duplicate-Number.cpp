b7ff48191dcd50915b696b39dbdb88d94d5521ea
Vikas Singh <vikas.technocrate@gmail.com>
2025-04-06 01:48:51 +0530

    [287] [Time Beats: 100.00%] [Memory Beats: 40.96%] - LeetPush

--- Find-the-Duplicate-Number.cpp
+++ Find-the-Duplicate-Number.cpp
@@ -1,14 +1,34 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        unordered_set<int>set;
 
-        for(int val : nums){
-            if(set.find(val) != set.end()){
-                return val;
-            }
-            set.insert(val);
+
+        //Approach -->> 1 -->>  Using Unordered Set -->>
+
+        // unordered_set<int>set;
+        // for(int val : nums){
+        //     if(set.find(val) != set.end()){
+        //         return val;
+        //     }
+        //     set.insert(val);
+        // }
+        // return -1;
+
+        //Approach -->> 2 -->> Slow & Fast Pointer Approach -->>
+
+        int slow  = nums[0];
+        int fast  = nums[0];
+
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+
+        slow = nums[0];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return -1;
+        return slow;
     }
 };
