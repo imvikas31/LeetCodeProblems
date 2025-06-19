edccb2b93bd8fb04cde1500bfebc7b43cba145a7
Vikas Singh <vikas.technocrate@gmail.com>
2025-06-19 05:41:44 +0530

    [1] [Time Beats: 100.00%] [Memory Beats: 21.90%] - LeetPush

--- Two-Sum.cpp
+++ Two-Sum.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        // int index1,index2;
+            // for(int i =0; i<nums.size();i++){
+            //     for(int j = i+1; j<nums.size(); j++){
+            //         if(nums[i]+nums[j] == target){
+            //             index1 = i;
+            //             index2 = j;
+            //         }
+            //     }
+            // }
+            // return {index1,index2};
 
-        int index1,index2;
+        // Hasing -->>
 
-        for(int i =0; i<nums.size();i++){
-            for(int j = i+1; j<nums.size(); j++){
-                if(nums[i]+nums[j] == target){
-                    index1 = i;
-                    index2 = j;
-                }
-            }
+        unordered_map<int,int>map;
+        vector<int>ans;
+
+        for(int i= 0; i < nums.size(); i++){
+            int first = nums[i];
+            int second = target-first;
+
+            if(map.find(second) != map.end()){
+                ans.push_back(i);
+                ans.push_back(map[second]);
+                break;
+            }       
+             map[first] = i; 
         }
-        return {index1,index2};
-    }
-        
+    return ans;
+
+}
+
 };
